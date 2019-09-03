4f82398c7bcb1da940f5cd1b3cfe85b60e7caa91
Fernando Guerra <fernandoguerra@Fernandos-MacBook-Pro.local>
2019-09-03 15:10:36 +0200

    Example Spring JPA

---
+++ src/main/java/com/apprentice/demo/UserService.java
@@ -0,0 +1,15 @@
+package com.apprentice.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Integer id) {
+        return userRepository.findById(id).get();
+    }
+}
