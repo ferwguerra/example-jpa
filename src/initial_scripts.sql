31a59ad93dd5a4e131ccf7623f793c9f177403f1
Fernando Guerra <fernandoguerra@Fernandos-MacBook-Pro.local>
2019-09-04 10:06:06 +0200

    updated scripts for courses

--- src/initial_scripts.sql
+++ src/initial_scripts.sql
@@ -58,3 +58,54 @@ VALUES
 "Gonzalo",
 35,
 2);
+
+CREATE TABLE `users`.`courses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `subject` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `users`.`users_courses` (
+`course_id` INT NOT NULL,
+`user_id` INT NOT NULL,
+PRIMARY KEY (`course_id`, `user_id`));
+
+INSERT INTO `users`.`courses`
+(
+`subject`)
+VALUES
+(
+'Maths');
+
+INSERT INTO `users`.`courses`
+(
+`subject`)
+VALUES
+(
+'Chemistry');
+
+ALTER TABLE users_courses
+ADD FOREIGN KEY (course_id) REFERENCES courses(id);
+
+ALTER TABLE users_courses
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+INSERT INTO `users`.`users_courses`
+(`course_id`,
+`user_id`)
+VALUES
+(1,
+1);
+
+INSERT INTO `users`.`users_courses`
+(`course_id`,
+`user_id`)
+VALUES
+(2,
+1);
+
+INSERT INTO `users`.`users_courses`
+(`course_id`,
+`user_id`)
+VALUES
+(1,
+2);
